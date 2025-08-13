6eb35cac7a538b1f92d7e9d5a6a2e33d9fab7190
Hilmi Ege Kara <hegekara48@gmail.com>
2025-08-13 15:18:02 +0300

    added order status enum

--- Backend/src/Backend.Api/Models/Order.cs
+++ Backend/src/Backend.Api/Models/Order.cs
@@ -1,11 +1,20 @@
 namespace Backend.Api.Models;
 
+public enum OrderStatus
+{
+    Ordered,
+    Pending,
+    Delivery,
+    Completed,
+    Aborted
+}
+
 public class Order
 {
     public int Id { get; set; }
     public DateTime OrderDate { get; set; }
     public DateTime? ShippedDate { get; set; }
-    public string Status { get; set; } = "Pending";
+    public OrderStatus Status { get; set; } = OrderStatus.Ordered;
     public bool? Deleted { get; set; }
 
     public int CustomerId { get; set; }
