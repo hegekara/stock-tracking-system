a60c6914df217f02feeb8cadcd1ea6d47f8dba88
Hilmi Ege Kara <hegekara48@gmail.com>
2025-08-14 11:48:24 +0300

    frontend init

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <div className="container text-center m-5">
+        <h1 className="display-4">CRM Sistemi</h1>
+        <p className="lead">Müşteri ilişkileri yönetimi.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
