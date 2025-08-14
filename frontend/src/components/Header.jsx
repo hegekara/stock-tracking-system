2e3f77c9759e61d3d6210703eb6b591e24e103a7
Hilmi Ege Kara <hegekara48@gmail.com>
2025-08-14 11:54:10 +0300

    created header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Header = () => {
+    const handleLogout = () => {
+        localStorage.clear();
+        window.location.reload();
+    };
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+            <div className="container-fluid">
+                <a className="navbar-brand" href='/home'>Stok Yönetimi</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between w-100">
+                    <ul className="navbar-nav flex-grow-1">
+                        <li className="nav-item">
+                            <a className="nav-link text-dark" href="/products">Ürünler</a>
+                        </li>
+                    </ul>
+                    <button className="btn btn-outline-danger" onClick={handleLogout}>
+                        Çıkış Yap
+                    </button>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Home() {
-  return (
-    <div>
-      <div className="container text-center m-5">
-        <h1 className="display-4">Stok Takip Sistemi</h1>
-        <p className="lead">Stok takibini kolaylaştır.</p>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <div className="container text-center m-5">
+                <h1 className="display-4">Stok Takip Sistemi</h1>
+                <p className="lead">Stok takibini kolaylaştır.</p>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
